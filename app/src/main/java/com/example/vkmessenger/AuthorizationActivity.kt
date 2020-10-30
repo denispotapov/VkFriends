01618c6c7065002690c3c91496bae17a0df3f889
Denis Potapov <fantom.r45@mail.ru>
2020-10-30 11:29:42 +0500

    authorization layout created

---
+++ app/src/main/java/com/example/vkmessenger/AuthorizationActivity.kt
@@ -0,0 +1,14 @@
+package com.example.vkmessenger
+
+import android.content.Intent
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import kotlinx.android.synthetic.main.activity_authorization.*
+
+class AuthorizationActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_authorization)
+
+    }
+}
